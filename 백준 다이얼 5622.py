c772987c3154f505e71801ec94725f0393a52372
nrsk1001 <85383119+nrsk1001@users.noreply.github.com>
2021-12-04 14:54:07 +0900

    Create 백준 다이얼 5622.py

---
+++ 백준 다이얼 5622.py
@@ -0,0 +1,58 @@
+a = input()
+b = 0
+for x in range(len(a)):
+
+    if a[x] == "A":
+        b = b+3
+    if a[x] == "B":
+        b = b+3
+    if a[x] == "C":
+        b = b+3
+    if a[x] == "D":
+        b = b+4
+    if a[x] == "E":
+        b = b+4
+    if a[x] == "F":
+        b = b+4
+    if a[x] == "G":
+        b = b+5
+    if a[x] == "H":
+        b = b + 5
+    if a[x] == "I":
+        b = b+5
+    if a[x] == "J":
+        b = b+6
+    if a[x] == "K":
+        b = b+6
+    if a[x] == "L":
+        b = b+6
+    if a[x] == "M":
+        b = b+7
+    if a[x] == "N":
+        b = b+7
+    if a[x] == "O":
+        b = b+7
+    if a[x] == "P":
+        b = b+8
+    if a[x] == "Q":
+        b = b+8
+    if a[x] == "R":
+        b = b+8
+    if a[x] == "S":
+        b = b+8
+    if a[x] == "T":
+        b = b+9
+    if a[x] == "U":
+        b = b+9
+    if a[x] == "V":
+        b = b+9
+    if a[x] == "W":
+        b = b+10
+    if a[x] == "X":
+        b = b+10
+    if a[x] == "Y":
+        b = b+10
+    if a[x] == "Z":
+        b = b+10
+
+print(b)
